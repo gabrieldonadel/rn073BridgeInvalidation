715f1a1d5619a8770093b37641a7912982328446
Gabriel Donadel <donadeldev@gmail.com>
2023-12-22 14:48:21 -0300

    Add event switch

--- ios/rn073BridgeInvalidation/AppDelegate.h
+++ ios/rn073BridgeInvalidation/AppDelegate.h
@@ -3,5 +3,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) UISwitch *switchControl;
+@property (nonatomic, strong) UILabel *switchLabel;
 
 @end
--- ios/rn073BridgeInvalidation/AppDelegate.mm
+++ ios/rn073BridgeInvalidation/AppDelegate.mm
@@ -12,37 +12,59 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
   UIViewController *viewController = [[UIViewController alloc] initWithNibName:nil bundle:nil];
+  viewController.view.backgroundColor = [UIColor whiteColor];
   self.window.rootViewController = viewController;
-  
+
   [self addButtonToView:viewController.view];
-  
+
   reactNativeViewController = [[ReactNativeViewController alloc] init];
   [reactNativeViewController initializeReactNativeApp];
-  
-  reactNativeViewController.vc.view.frame = CGRectMake(0, 120, self.window.bounds.size.width, self.window.bounds.size.height - 100);
+
+  reactNativeViewController.vc.view.frame = CGRectMake(0, 135, self.window.bounds.size.width, self.window.bounds.size.height - 100);
   [viewController.view addSubview:reactNativeViewController.vc.view];
-  
+
   [self.window makeKeyAndVisible];
-  
+
   return YES;
 }
 
 - (void)addButtonToView:(UIView *)view  {
+  self.switchControl = [[UISwitch alloc] init];
+  [view addSubview:self.switchControl];
+
+  self.switchLabel = [[UILabel alloc] init];
+  self.switchLabel.text = @"Send UserInterfaceStyle event after invalidating";
+  self.switchLabel.font = [UIFont systemFontOfSize:14];
+  [view addSubview:self.switchLabel];
+
+  self.switchControl.translatesAutoresizingMaskIntoConstraints = NO;
+  self.switchLabel.translatesAutoresizingMaskIntoConstraints = NO;
+
+  [NSLayoutConstraint activateConstraints:@[
+    [self.switchControl.leadingAnchor constraintEqualToAnchor:view.leadingAnchor constant:10],
+    [self.switchControl.topAnchor constraintEqualToAnchor:view.topAnchor constant:50],
+
+    [self.switchLabel.leadingAnchor constraintEqualToAnchor:self.switchControl.trailingAnchor constant:10],
+    [self.switchLabel.centerYAnchor constraintEqualToAnchor:self.switchControl.centerYAnchor],
+  ]];
+
   UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
   [button setTitle:@"Invalidate and create new instance" forState:UIControlStateNormal];
   [button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
   [button sizeToFit];
-  button.center = CGPointMake(self.window.bounds.size.width / 2, 80);
-  
+  button.center = CGPointMake(self.window.bounds.size.width / 2, 105);
+
   [view addSubview:button];
 }
 
 - (void)buttonTapped:(UIButton *)sender {
   dispatch_async(dispatch_get_main_queue(), ^{
     [self->reactNativeViewController invalidate];
-    
+
     [self->reactNativeViewController   initializeReactNativeApp];
-    //    [self sendUserInterfaceStyleNotification];
+    if(self.switchControl.isOn){
+      [self sendUserInterfaceStyleNotification];
+    }
   });
 }
 
@@ -50,7 +72,7 @@
 {
   UIWindow *mainWindow = [[UIApplication sharedApplication] keyWindow];
   id<UITraitEnvironment>env = mainWindow.rootViewController;
-  
+
   [[NSNotificationCenter defaultCenter] postNotificationName:RCTUserInterfaceStyleDidChangeNotification
                                                       object:env
                                                     userInfo:@{
